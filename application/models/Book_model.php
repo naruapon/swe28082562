2de1dd29225362853074155cebb27e4ab43c7762
naruapon <naruapon@webmail.npru.ac.th>
2019-08-28 17:42:24 +0700

    Insert Object

---
+++ application/models/Book_model.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Book_model extends CI_Model {
+        var $code = "P005";
+        var $name = "Java Script";
+        var $price = 299.25;
+        var $cid = 1;
+}
